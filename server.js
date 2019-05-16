3e7e0a1db9f63ee48ed312a43d88428a2863b36e
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2019-05-16 13:18:36 -0300

    Creating product model

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
 
 app.get('/', (req, res) => {
-  res.send('Node.js');
+  Product.create({
+    title: 'React Native',
+    description: 'Build native mobile apps',
+    url: 'https://github.com/facebook/react-native'
+  });
+
+  return res.send('Node.js');
 });
 
 app.listen(3001);
